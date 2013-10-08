a0ea67f2efbdef54e0f07c9dd902bffdebb4b449
Surya Gaddipati <sgaddipati@groupon.com>
2013-10-08 18:01:30 -0500

    Added cycle notation

--- programs/permutations.rb
+++ programs/permutations.rb
@@ -1,10 +1,31 @@
 
 
-def print(p)
+def print_p(p)
   puts '-'*10
   p ((1..p.to_a.size).to_a)
   p p.to_a
   puts '-'*10
+end
+
+def print_cycle(p)
+  looped_elements = []
+  begin
+  looped_elements <<  print_loop_for((p - looped_elements.flatten)[0],p.to_a)
+
+  end while( looped_elements.flatten.size != p.to_a.size)
+  puts ""
+end
+
+def print_loop_for(current_element,p)
+  looped_elements = []
+  print "("
+  begin
+    print current_element
+    looped_elements << current_element
+    current_element = p [current_element -1]
+  end while(!looped_elements.include?(current_element))
+  print ")"
+  looped_elements
 end
 
 def goes_to(e, cycle)
@@ -18,9 +39,12 @@ end
 
 
 p = (1..4) 
-print(p)
-(1..3).each{|x|p = apply_permutation(p,[1,2,3,4])}
-print(p)
+print_p(p)
+(1..3).each{|x|
+  p = apply_permutation(p,[3,4])
+print_p(p)
+print_cycle(p)
+}
 #apply_permutation(p,1,6)
 #apply_permutation(p,6,4,2,5,9)
 #apply_permutation(p,2,4,9,7)
